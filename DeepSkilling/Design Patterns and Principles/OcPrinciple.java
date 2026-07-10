1c2d85b46638a9f478d10737c5788c29d320eb85
Varun Marineni <varunmarineni005@gmail.com>
2026-07-10 13:18:26 +0530

    Add design principles

---
+++ DeepSkilling/Design Patterns and Principles/OcPrinciple.java
@@ -0,0 +1,40 @@
+package DeepSkilling;
+
+public class OcPrinciple {
+    public static void main(String[] args) {
+
+        Shape circle = new Circle();
+        circle.draw();
+
+        Shape rectangle = new Rectangle();
+        rectangle.draw();
+
+        Shape triangle = new Triangle();
+        triangle.draw();
+    }
+}
+
+abstract class Shape {
+    abstract void draw();
+}
+
+class Circle extends Shape {
+    @Override
+    void draw() {
+        System.out.println("Drawing Circle");
+    }
+}
+
+class Rectangle extends Shape {
+    @Override
+    void draw() {
+        System.out.println("Drawing Rectangle");
+    }
+}
+
+class Triangle extends Shape {
+    @Override
+    void draw() {
+        System.out.println("Drawing Triangle");
+    }
+}
